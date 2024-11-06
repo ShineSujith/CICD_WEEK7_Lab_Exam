feee0dd3058e399db1b41a5472bb6db77d468ebc
Shine Sujith <G00414454@atu.ie>
2024-11-06 14:16:48 +0000

    added code to RentalController

--- src/main/java/ie/atu/week7_labexam/RentalController.java
+++ src/main/java/ie/atu/week7_labexam/RentalController.java
@@ -1,4 +1,25 @@
 package ie.atu.week7_labexam;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class RentalController {
+    @PostMapping("/rentals")
+    public List<Rental> addRental(@RequestBody Rental rental) {
+
+    }
+    @GetMapping("/rentals/{rentalCode}")
+    public List<Rental> getRental(String rentalCode) {
+
+    }
+    @PutMapping("/rentals/{rentalCode}")
+    public List<Rental> changeRental(@RequestBody Rental rental, String rentalCode) {
+
+    }
+    @DeleteMapping("/rentals/{rentalCode}")
+    public List<Rental> deleteRental(@RequestBody Rental rental, String rentalCode) {
+
+    }
 }
--- src/main/java/ie/atu/week7_labexam/RentalService.java
+++ src/main/java/ie/atu/week7_labexam/RentalService.java
@@ -1,4 +1,8 @@
 package ie.atu.week7_labexam;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RentalService {
+
 }
